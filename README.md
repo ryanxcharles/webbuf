56d410615550fe7dc9215be842cae92b1e929a23
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-22 14:47:26 -0500

    test base64-js and ieee754

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # WebBuf
 
-WebBuf is a re-export of the NPM package `buffer` with a different name. The
-purpose is to prohibit use of the global `Buffer` object throughout your code
-base for any code wants to use the `buffer` package in the browser and wants to
-ensure that the global `Buffer` object is not used.
+WebBuf is a fork of the NPM package [`buffer`](https://github.com/feross/buffer)
+with a different name. The purpose is to prohibit use of the global `Buffer`
+object throughout your code base for any code wants to use the `buffer` package
+in the browser and wants to ensure that the global `Buffer` object is not used.
+Also, it uses ES Modules instead of CommonJS.
 
 Usage:
 
 ```javascript
-import { WebBuf } from 'webbuf';
+import { WebBuf } from "webbuf";
 
-const buf = WebBuf.from('Hello, world!');
-console.log(buf.toString('utf8'));
+const buf = WebBuf.from("Hello, world!");
+console.log(buf.toString("utf8"));
 ```
 
 The interface, of course, is the same as the `buffer` package.
--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,3 @@
-// import vitest
 import { describe, it, expect } from "vitest";
 import { WebBuf } from "./index.js";
 
@@ -7,5 +6,33 @@ describe("WebBuf", () => {
     const buf = WebBuf.alloc(10);
     expect(buf).toBeInstanceOf(WebBuf);
     expect(WebBuf.isBuffer(buf)).toBe(true);
+  });
+
+  it("should encode and decode base64", () => {
+    const myStr = "Hello, World!";
+    const buf = WebBuf.from(myStr);
+    const base64 = buf.toString("base64");
+    const decoded = WebBuf.from(base64, "base64");
+    expect(decoded.toString()).toBe(myStr);
+  });
+  
+  it("should encode and decode hex", () => {
+    const myStr = "Hello, World!";
+    const buf = WebBuf.from(myStr);
+    const hex = buf.toString("hex");
+    const decoded = WebBuf.from(hex, "hex");
+    expect(decoded.toString()).toBe(myStr);
+  });
+
+  it('should read and wrote a float in little endian', () => {
+    const buf = WebBuf.alloc(4);
+    buf.writeFloatLE(3.14, 0);
+    expect(buf.readFloatLE(0)).toBeCloseTo(3.14);
+  });
+
+  it('should read and wrote a float in big endian', () => {
+    const buf = WebBuf.alloc(4);
+    buf.writeFloatBE(3.14, 0);
+    expect(buf.readFloatBE(0)).toBeCloseTo(3.14);
   });
 });
