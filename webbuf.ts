61633d1a0dbdcb178440c4eecea6c316ef0e37af
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-22 16:53:06 -0500

    to/from methods

--- webbuf.ts
+++ webbuf.ts
@@ -1,31 +1,70 @@
 import * as base64 from "./base64-js.js";
 import * as ieee754 from "./ieee754.js";
 
-export class WebBuf {
-  buffer: Uint8Array;
-  // constructor
-  constructor(size: number) {
-    this.buffer = new Uint8Array(size);
-  }
+export class WebBuf extends Uint8Array {
+  // constructor(size: number) {
+  //   super(size);
+  // }
   // concat
+
   static concat(list: Uint8Array[]) {
-    let size = 0;
-    for (let i = 0; i < list.length; i++) {
-      size += list[i].length;
-    }
-    let buffer = new Uint8Array(size);
+    const size = list.reduce((acc, buf) => acc + buf.length, 0);
+    const result = new WebBuf(size);
     let offset = 0;
-    for (let i = 0; i < list.length; i++) {
-      buffer.set(list[i], offset);
-      offset += list[i].length;
+    for (const buf of list) {
+      result.set(buf, offset);
+      offset += buf.length;
     }
-    return buffer;
+    return result;
   }
-  // alloc
+
   static alloc(size: number) {
     return new WebBuf(size);
   }
-  // from (string, hex, base64)
+
+  static fromUint8Array(buffer: Uint8Array) {
+    return new WebBuf(buffer);
+  }
+
+  static fromArray(array: number[]) {
+    return new WebBuf(array);
+  }
+
+  static fromString(str: string) {
+    const encoder = new TextEncoder();
+    return new WebBuf(encoder.encode(str));
+  }
+
+  static fromHex(hex: string) {
+    const result = new WebBuf(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+      result[i / 2] = Number.parseInt(hex.slice(i, i + 2), 16);
+    }
+    return result;
+  }
+
+  static fromBase64(b64: string) {
+    const byteArray = base64.toByteArray(b64);
+    return new WebBuf(byteArray);
+  }
+  
+  toString() {
+    const decoder = new TextDecoder();
+    return decoder.decode(this);
+  }
+
+  toBase64() {
+    return base64.fromByteArray(this);
+  }
+
+  toHex() {
+    return Array.from(this).map((byte) => byte.toString(16).padStart(2, "0")).join("");
+  }
+
+  toArray() {
+    return Array.from(this);
+  }
+
   // toString
   // fromString
   // slice
