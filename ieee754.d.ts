8551379579f8d29f8d629ce75c297b1ee171bd33
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-22 14:16:56 -0500

    import ieee754

---
+++ ieee754.d.ts
@@ -0,0 +1,15 @@
+export function read(
+  buffer: Uint8Array,
+  offset: number,
+  isLE: boolean,
+  mLen: number,
+  nBytes: number,
+): number;
+export function write(
+  buffer: Uint8Array,
+  value: number,
+  offset: number,
+  isLE: boolean,
+  mLen: number,
+  nBytes: number,
+): void;
