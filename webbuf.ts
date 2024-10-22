965f18b0f958e9e322de9421174576ff087bba0b
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-22 16:39:17 -0500

    constructor. concat. alloc.

--- webbuf.ts
+++ webbuf.ts
@@ -1,2 +1,34 @@
+import * as base64 from "./base64-js.js";
+import * as ieee754 from "./ieee754.js";
+
 export class WebBuf {
+  buffer: Uint8Array;
+  // constructor
+  constructor(size: number) {
+    this.buffer = new Uint8Array(size);
+  }
+  // concat
+  static concat(list: Uint8Array[]) {
+    let size = 0;
+    for (let i = 0; i < list.length; i++) {
+      size += list[i].length;
+    }
+    let buffer = new Uint8Array(size);
+    let offset = 0;
+    for (let i = 0; i < list.length; i++) {
+      buffer.set(list[i], offset);
+      offset += list[i].length;
+    }
+    return buffer;
+  }
+  // alloc
+  static alloc(size: number) {
+    return new WebBuf(size);
+  }
+  // from (string, hex, base64)
+  // toString
+  // fromString
+  // slice
+  // subarray
+  // read/write binary numbers
 }
