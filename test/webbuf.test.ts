c0a2c45dff9f7bcc3c2091648ce8656967265b9f
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-23 03:47:44 -0500

    throws on nonvalid characters

--- test/webbuf.test.ts
+++ test/webbuf.test.ts
@@ -12,17 +12,21 @@ describe("WebBuf", () => {
       expect(decoded.toString()).toBe(myStr);
     });
 
-    it('should encode and decode arbitrary binary data', () => {
-      const hex = "000102030405060708090a0b0c0d0e0ff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+    it("should encode and decode arbitrary binary data", () => {
+      const hex =
+        "000102030405060708090a0b0c0d0e0ff0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
       const buf = WebBuf.fromHex(hex);
       const base64 = buf.toBase64();
       const decoded = WebBuf.fromBase64(base64);
       expect(decoded.toHex()).toBe(hex);
     });
 
-    it('should encode and decode arbitrary binary data of length n, n+1, n+2 ... m', () => {
+    it("should encode and decode arbitrary binary data of length n, n+1, n+2 ... m", () => {
       for (let i = 0; i < 100; i++) {
-        const hex = new Array(i).fill(0).map((_, i) => i.toString(16).padStart(2, "0")).join("");
+        const hex = new Array(i)
+          .fill(0)
+          .map((_, i) => i.toString(16).padStart(2, "0"))
+          .join("");
         const buf = WebBuf.fromHex(hex);
         const base64 = buf.toBase64();
         const decoded = WebBuf.fromBase64(base64);
@@ -43,35 +47,48 @@ describe("WebBuf", () => {
 
   describe("base64", () => {
     // t.equal((new B('YW9ldQ', 'base64').toString()), 'aoeu')
-    it('should decode base64', () => {
+    it("should decode base64", () => {
       const base64 = "YW9ldQ==";
       const decoded = WebBuf.fromBase64(base64);
-      expect(decoded.toString()).toBe('aoeu');
+      expect(decoded.toString()).toBe("aoeu");
     });
 
-    it('should ignore whitespace', () => {
-  // const text = '\n   YW9ldQ==  '
-  // const buf = new B(text, 'base64')
-  // t.equal(buf.toString(), 'aoeu')
+    it("should ignore whitespace", () => {
+      // const text = '\n   YW9ldQ==  '
+      // const buf = new B(text, 'base64')
+      // t.equal(buf.toString(), 'aoeu')
       const base64 = "\n   YW9ldQ==  ";
       const decoded = WebBuf.fromBase64(base64);
-      expect(decoded.toString()).toBe('aoeu');
+      expect(decoded.toString()).toBe("aoeu");
     });
 
-    it('should handle newline in utf8', () => {
-    // new B('LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK', 'base64').toString('utf8'),
-    // '---\ntitle: Three dashes marks the spot\ntags:\n'
-      const base64 = "LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK";
+    it("should handle newline in utf8", () => {
+      // new B('LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK', 'base64').toString('utf8'),
+      // '---\ntitle: Three dashes marks the spot\ntags:\n'
+      const base64 =
+        "LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK";
       const decoded = WebBuf.fromBase64(base64);
-      expect(decoded.toString()).toBe('---\ntitle: Three dashes marks the spot\ntags:\n');
+      expect(decoded.toString()).toBe(
+        "---\ntitle: Three dashes marks the spot\ntags:\n",
+      );
     });
 
-    it('should strip newline in base64', () => {
-    // new B('LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK\nICAtIHlhbWwKICAtIGZyb250LW1hdHRlcgogIC0gZGFzaGVzCmV4cGFuZWQt', 'base64').toString('utf8'),
-    // '---\ntitle: Three dashes marks the spot\ntags:\n  - yaml\n  - front-matter\n  - dashes\nexpaned-'
-      const base64 = "LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoKICAtIHlhbWwKICAtIGZyb250LW1hdHRlcgogIC0gZGFzaGVzCmV4cGFuZWQt";
+    it("should strip newline in base64", () => {
+      // new B('LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoK\nICAtIHlhbWwKICAtIGZyb250LW1hdHRlcgogIC0gZGFzaGVzCmV4cGFuZWQt', 'base64').toString('utf8'),
+      // '---\ntitle: Three dashes marks the spot\ntags:\n  - yaml\n  - front-matter\n  - dashes\nexpaned-'
+      const base64 =
+        "LS0tCnRpdGxlOiBUaHJlZSBkYXNoZXMgbWFya3MgdGhlIHNwb3QKdGFnczoKICAtIHlhbWwKICAtIGZyb250LW1hdHRlcgogIC0gZGFzaGVzCmV4cGFuZWQt";
       const decoded = WebBuf.fromBase64(base64);
-      expect(decoded.toString()).toBe('---\ntitle: Three dashes marks the spot\ntags:\n  - yaml\n  - front-matter\n  - dashes\nexpaned-');
+      expect(decoded.toString()).toBe(
+        "---\ntitle: Three dashes marks the spot\ntags:\n  - yaml\n  - front-matter\n  - dashes\nexpaned-",
+      );
+    });
+
+    it("invalid non-alaphanumeric characters should throws", () => {
+      // new B('!"#$%&\'()*,.:;<=>?@[\\]^`{|}~', 'base64').toString('utf8'),
+      // ''
+      const base64 = "!\"#$%&'()*,.:;<=>?@[\\]^`{|}~";
+      expect(() => WebBuf.fromBase64(base64)).toThrow();
     });
   });
 });
