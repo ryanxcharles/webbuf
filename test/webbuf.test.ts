3f9e548b4647dc05af19e51437f23d9ecb92cd89
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-23 05:36:00 -0500

    i64be

--- test/webbuf.test.ts
+++ test/webbuf.test.ts
@@ -462,25 +462,25 @@ describe("WebBuf", () => {
         expect(buf.readBigInt64BE(0)).toBe(-1311768467463790320n);
       });
 
-      it.skip("should throw if writing a number that is too big", () => {
+      it("should throw if writing a number that is too big", () => {
         const buf = WebBuf.alloc(8);
         expect(() =>
           buf.writeBigInt64BE(-0x8000000000000000n - 1n, 0),
         ).toThrow();
       });
 
-      it.skip("should write a valid i64be with valid offset", () => {
+      it("should write a valid i64be with valid offset", () => {
         const buf = WebBuf.alloc(9);
         buf.writeBigInt64BE(-1311768467463790320n, 1);
         expect(buf.readBigInt64BE(1)).toBe(-1311768467463790320n);
       });
 
-      it.skip("should throw if offset is invalid", () => {
+      it("should throw if offset is invalid", () => {
         const buf = WebBuf.alloc(8);
         expect(() => buf.writeBigInt64BE(-1311768467463790320n, 1)).toThrow();
       });
 
-      it.skip("should write and read the biggest i64be", () => {
+      it("should write and read the biggest i64be", () => {
         const buf = WebBuf.alloc(8);
         buf.writeBigInt64BE(-0x8000000000000000n, 0);
         expect(buf.readBigInt64BE(0)).toBe(-0x8000000000000000n);
