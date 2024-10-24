c5601b6ebd869ea044c86d77104c64f26de50c33
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-24 08:03:33 -0500

    enable browser tests by default

--- ts/vitest.config.js
+++ ts/vitest.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   // Enable for benchmarks
-  // test: {
-  //   browser: {
-  //     enabled: true,
-  //     provider: 'playwright',
-  //     name: 'chromium',
-  //     headless: true,
-  //   },
-  // },
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      name: 'chromium',
+      headless: true,
+    },
+  },
 });
