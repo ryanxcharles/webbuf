9c438aa01c9b56204c07d4e658b09d401e92cff3
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-24 06:39:15 -0500

    Revert "new rust build"
    This reverts commit b3dcfbc253e93d6009f5c35f48fd2cbcf9baaa56.

--- rs/build/bundler/webbuf.d.ts
+++ rs/build/bundler/webbuf.d.ts
@@ -10,9 +10,9 @@ export function encode_base64(data: Uint8Array): string;
  * Decode a base64 string into a byte vector
  * Returns an error string if decoding fails
  * @param {string} encoded
- * @returns {string}
+ * @returns {Uint8Array}
  */
-export function decode_base64(encoded: string): string;
+export function decode_base64(encoded: string): Uint8Array;
 /**
  * Encode a byte slice into a hex string
  * @param {Uint8Array} data
@@ -20,9 +20,9 @@ export function decode_base64(encoded: string): string;
  */
 export function encode_hex(data: Uint8Array): string;
 /**
- * Decode a hex string into a regular UTF-8 string
- * Returns an error string if decoding fails or if the decoded data is not valid UTF-8
+ * Decode a hex string into a byte vector
+ * Returns an error string if decoding fails
  * @param {string} encoded
- * @returns {string}
+ * @returns {Uint8Array}
  */
-export function decode_hex(encoded: string): string;
+export function decode_hex(encoded: string): Uint8Array;
