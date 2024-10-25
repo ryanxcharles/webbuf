ffa946a5772f3ea9f20df4927bcf3a73fc1b4ab0
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-25 08:41:31 -0500

    better build tool here too. rebuild.

--- ts/webbuf-blake3/src/rs-webbuf_blake3-inline-base64/webbuf_blake3.js
+++ ts/webbuf-blake3/src/rs-webbuf_blake3-inline-base64/webbuf_blake3.js
@@ -1,4 +1,4 @@
-import * as wasm from "./webbuf_blake3_bg.wasm";
+import { wasm } from "./webbuf_blake3_bg.wasm.js";
 export * from "./webbuf_blake3_bg.js";
 import { __wbg_set_wasm } from "./webbuf_blake3_bg.js";
 __wbg_set_wasm(wasm);
