5e7ebac0ecbd06d64040880ed276e3fbc1690ea6
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-25 08:28:12 -0500

    rebuild webbuf

--- ts/webbuf/src/rs-webbuf-bundler/webbuf_bg.js
+++ ts/webbuf/src/rs-webbuf-bundler/webbuf_bg.js
@@ -6,7 +6,7 @@ export function __wbg_set_wasm(val) {
 
 const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
 
-const cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
 
 cachedTextDecoder.decode();
 
@@ -31,7 +31,7 @@ heap.push(undefined, null, true, false);
 let heap_next = heap.length;
 
 function addHeapObject(obj) {
-    if (heap_next === heap.length) { heap.push(heap.length + 1); }
+    if (heap_next === heap.length) heap.push(heap.length + 1);
     const idx = heap_next;
     heap_next = heap[idx];
 
@@ -69,8 +69,8 @@ export function encode_base64(data) {
         const ptr0 = passArray8ToWasm0(data, wasm.__wbindgen_malloc);
         const len0 = WASM_VECTOR_LEN;
         wasm.encode_base64(retptr, ptr0, len0);
-        const r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        const r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+        var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         deferred2_0 = r0;
         deferred2_1 = r1;
         return getStringFromWasm0(r0, r1);
@@ -82,18 +82,20 @@ export function encode_base64(data) {
 
 const lTextEncoder = typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder;
 
-const cachedTextEncoder = new lTextEncoder('utf-8');
+let cachedTextEncoder = new lTextEncoder('utf-8');
 
 const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
-    ? ((arg, view) => cachedTextEncoder.encodeInto(arg, view))
-    : ((arg, view) => {
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
     const buf = cachedTextEncoder.encode(arg);
     view.set(buf);
     return {
         read: arg.length,
         written: buf.length
     };
-}));
+});
 
 function passStringToWasm0(arg, malloc, realloc) {
 
@@ -114,7 +116,7 @@ function passStringToWasm0(arg, malloc, realloc) {
 
     for (; offset < len; offset++) {
         const code = arg.charCodeAt(offset);
-        if (code > 0x7F) { break; }
+        if (code > 0x7F) break;
         mem[ptr + offset] = code;
     }
 
@@ -137,7 +139,7 @@ function passStringToWasm0(arg, malloc, realloc) {
 function getObject(idx) { return heap[idx]; }
 
 function dropObject(idx) {
-    if (idx < 132) { return; }
+    if (idx < 132) return;
     heap[idx] = heap_next;
     heap_next = idx;
 }
@@ -164,14 +166,14 @@ export function decode_base64_strip_whitespace(encoded) {
         const ptr0 = passStringToWasm0(encoded, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         const len0 = WASM_VECTOR_LEN;
         wasm.decode_base64_strip_whitespace(retptr, ptr0, len0);
-        const r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        const r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
-        const r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
-        const r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
+        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+        var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+        var r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
+        var r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
         if (r3) {
             throw takeObject(r2);
         }
-        const v2 = getArrayU8FromWasm0(r0, r1).slice();
+        var v2 = getArrayU8FromWasm0(r0, r1).slice();
         wasm.__wbindgen_free(r0, r1 * 1, 1);
         return v2;
     } finally {
@@ -189,14 +191,14 @@ export function decode_base64(encoded) {
         const ptr0 = passStringToWasm0(encoded, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         const len0 = WASM_VECTOR_LEN;
         wasm.decode_base64(retptr, ptr0, len0);
-        const r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        const r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
-        const r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
-        const r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
+        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+        var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+        var r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
+        var r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
         if (r3) {
             throw takeObject(r2);
         }
-        const v2 = getArrayU8FromWasm0(r0, r1).slice();
+        var v2 = getArrayU8FromWasm0(r0, r1).slice();
         wasm.__wbindgen_free(r0, r1 * 1, 1);
         return v2;
     } finally {
@@ -217,8 +219,8 @@ export function encode_hex(data) {
         const ptr0 = passArray8ToWasm0(data, wasm.__wbindgen_malloc);
         const len0 = WASM_VECTOR_LEN;
         wasm.encode_hex(retptr, ptr0, len0);
-        const r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        const r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+        var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         deferred2_0 = r0;
         deferred2_1 = r1;
         return getStringFromWasm0(r0, r1);
@@ -240,14 +242,14 @@ export function decode_hex(encoded) {
         const ptr0 = passStringToWasm0(encoded, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         const len0 = WASM_VECTOR_LEN;
         wasm.decode_hex(retptr, ptr0, len0);
-        const r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
-        const r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
-        const r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
-        const r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
+        var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
+        var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
+        var r2 = getDataViewMemory0().getInt32(retptr + 4 * 2, true);
+        var r3 = getDataViewMemory0().getInt32(retptr + 4 * 3, true);
         if (r3) {
             throw takeObject(r2);
         }
-        const v2 = getArrayU8FromWasm0(r0, r1).slice();
+        var v2 = getArrayU8FromWasm0(r0, r1).slice();
         wasm.__wbindgen_free(r0, r1 * 1, 1);
         return v2;
     } finally {
