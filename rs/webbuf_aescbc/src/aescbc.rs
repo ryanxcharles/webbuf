81a6b74adcc717a59f95ee184e9c5af4c07b21db
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-25 17:23:56 -0500

    complete cbc implementation. needs tests.

--- rs/webbuf_aescbc/src/aescbc.rs
+++ rs/webbuf_aescbc/src/aescbc.rs
@@ -1,13 +1,11 @@
-use std::convert::TryInto;
-use crate::aes::{aes_encrypt, aes_decrypt};
-
-// Ensure the aes_encrypt and aes_decrypt functions are already defined as:
-// fn aes_encrypt(key: &[u8], block: &[u8]) -> Vec<u8>;
-// fn aes_decrypt(key: &[u8], block: &[u8]) -> Vec<u8>;
+use crate::aes::{aes_decrypt, aes_encrypt};
 
 // Helper function to XOR two byte slices
 fn xor_bufs(buf1: &[u8], buf2: &[u8]) -> Vec<u8> {
-    buf1.iter().zip(buf2.iter()).map(|(&x1, &x2)| x1 ^ x2).collect()
+    buf1.iter()
+        .zip(buf2.iter())
+        .map(|(&x1, &x2)| x1 ^ x2)
+        .collect()
 }
 
 // PKCS#7 Padding
@@ -45,7 +43,7 @@ fn blocks_to_buf(blocks: Vec<Vec<u8>>) -> Vec<u8> {
     let mut buf = vec![];
     for (i, block) in blocks.iter().enumerate() {
         if i == blocks.len() - 1 {
-            buf.extend(pkcs7_unpad(&block));
+            buf.extend(pkcs7_unpad(block));
         } else {
             buf.extend(block);
         }
@@ -53,56 +51,50 @@ fn blocks_to_buf(blocks: Vec<Vec<u8>>) -> Vec<u8> {
     buf
 }
 
-// // AES-CBC Encrypt
-// pub fn encrypt_cbc(plaintext: &[u8], aes_key: &[u8], iv: &[u8], concat_iv: bool) -> Vec<u8> {
-//     let block_size = 16;
-    
-//     if iv.len() != block_size || ![16, 24, 32].contains(&aes_key.len()) {
-//         panic!("Invalid IV or key size");
-//     }
+// AES-CBC Encrypt
+pub fn encrypt_cbc(plaintext: &[u8], aes_key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let block_size = 16;
 
-//     let blocks = buf_to_blocks(plaintext, block_size);
-//     let mut ciphertext = vec![];
-//     let mut prev_block = iv.clone();
+    if iv.len() != block_size || ![16, 24, 32].contains(&aes_key.len()) {
+        panic!("Invalid IV or key size");
+    }
 
-//     for block in blocks {
-//         let xored = xor_bufs(&block, &prev_block);
-//         let encrypted_block = aes_encrypt(aes_key, &xored);
-//         ciphertext.extend(&encrypted_block);
-//         prev_block = encrypted_block;
-//     }
+    let blocks = buf_to_blocks(plaintext, block_size);
+    let mut ciphertext = vec![];
+    let mut prev_block = iv.to_vec();
 
-//     if concat_iv {
-//         [iv, &ciphertext].concat()
-//     } else {
-//         ciphertext
-//     }
-// }
+    for block in blocks {
+        let xored = xor_bufs(&block, &prev_block);
+        let encrypted_block = aes_encrypt(aes_key, &xored);
+        ciphertext.extend(&encrypted_block);
+        prev_block = encrypted_block;
+    }
 
-// // AES-CBC Decrypt
-// pub fn decrypt_cbc(ciphertext: &[u8], aes_key: &[u8], iv: Option<&[u8]>) -> Vec<u8> {
-//     let block_size = 16;
-//     let iv = match iv {
-//         Some(iv) => iv.to_vec(),
-//         None => ciphertext[..block_size].to_vec(),
-//     };
+    ciphertext
+}
 
-//     let ciphertext_blocks = if iv.len() == block_size {
-//         ciphertext[block_size..].chunks(block_size).map(|chunk| chunk.to_vec()).collect::<Vec<_>>()
-//     } else {
-//         panic!("Invalid IV size")
-//     };
+// AES-CBC Decrypt
+pub fn decrypt_cbc(ciphertext: &[u8], aes_key: &[u8], iv: &[u8]) -> Vec<u8> {
+    let block_size = 16;
 
-//     let mut plaintext_blocks = vec![];
-//     let mut prev_block = iv;
+    let ciphertext_blocks = if iv.len() == block_size {
+        ciphertext[block_size..]
+            .chunks(block_size)
+            .map(|chunk| chunk.to_vec())
+            .collect::<Vec<_>>()
+    } else {
+        panic!("Invalid IV size")
+    };
 
-//     for block in ciphertext_blocks {
-//         let decrypted_block = aes_decrypt(aes_key, &block);
-//         let plaintext_block = xor_bufs(&decrypted_block, &prev_block);
-//         plaintext_blocks.push(plaintext_block);
-//         prev_block = block;
-//     }
+    let mut plaintext_blocks = vec![];
+    let mut prev_block = iv.to_vec();
 
-//     blocks_to_buf(plaintext_blocks)
-// }
+    for block in ciphertext_blocks {
+        let decrypted_block = aes_decrypt(aes_key, &block);
+        let plaintext_block = xor_bufs(&decrypted_block, &prev_block);
+        plaintext_blocks.push(plaintext_block);
+        prev_block = block;
+    }
 
+    blocks_to_buf(plaintext_blocks)
+}
