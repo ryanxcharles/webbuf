71ffe636e5361b1ac27fa21ed3bd2e295bc9f200
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-25 14:18:44 -0500

    prepare to create tests

---
+++ ts/webbuf-secp256k1/src/index.ts
@@ -0,0 +1,1 @@
+export * from "./rs-webbuf_secp256k1-inline-base64/webbuf_secp256k1.js";
---
+++ ts/webbuf-secp256k1/test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { WebBuf } from "webbuf";
+import { FixedBuf } from "@webbuf/fixedbuf";
+
+// describe("secp256k1", () => {
+//   it("should correctly sign and verify a message", () => {
+    
+// });
