1e60f18f808df5804a689fe3289865dfa21c5d98
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-25 14:00:12 -0500

    secp256k1 build wasm

--- rs/webbuf_secp256k1/wasm-pack-bundler.zsh
+++ rs/webbuf_secp256k1/wasm-pack-bundler.zsh
@@ -2,3 +2,6 @@
 
 wasm-pack build --target bundler --out-dir build/bundler --release -- --features wasm
 rm build/bundler/.gitignore
+rm build/bundler/README.md
+rm build/bundler/LICENSE
+rm build/bundler/package.json
