1b1df325574bc009e917aa73be3f81d3a540b2da
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-26 13:55:12 -0500

    make api match other library

--- ts/webbuf-aescbc/src/index.ts
+++ ts/webbuf-aescbc/src/index.ts
@@ -6,35 +6,35 @@ import { WebBuf } from "@webbuf/webbuf";
 import { FixedBuf } from "@webbuf/fixedbuf";
 
 export function aescbDecrypt(
-  key: FixedBuf<32>,
-  data: WebBuf,
+  ciphertext: WebBuf,
+  aesKey: FixedBuf<32>,
   iv?: FixedBuf<16>,
 ): WebBuf {
   if (iv === undefined) {
-    if (data.length < 16) {
+    if (ciphertext.length < 16) {
       throw new Error("Data must be at least 16 bytes long");
     }
-    const iv = FixedBuf.fromBuf(16, data.slice(0, 16));
-    data = data.slice(16);
-    if (data.length % 16 !== 0) {
+    const iv = FixedBuf.fromBuf(16, ciphertext.slice(0, 16));
+    ciphertext = ciphertext.slice(16);
+    if (ciphertext.length % 16 !== 0) {
       throw new Error("Data length must be a multiple of 16");
     }
 
-    return WebBuf.fromUint8Array(aescbc_decrypt(data, key.buf, iv.buf));
+    return WebBuf.fromUint8Array(aescbc_decrypt(ciphertext, aesKey.buf, iv.buf));
   }
-  if (data.length % 16 !== 0) {
+  if (ciphertext.length % 16 !== 0) {
     throw new Error("Data length must be a multiple of 16");
   }
-  return WebBuf.fromUint8Array(aescbc_decrypt(data, key.buf, iv.buf));
+  return WebBuf.fromUint8Array(aescbc_decrypt(ciphertext, aesKey.buf, iv.buf));
 }
 
 export function aescbEncrypt(
-  key: FixedBuf<32>,
-  data: WebBuf,
+  plaintext: WebBuf,
+  aesKey: FixedBuf<32>,
   iv: FixedBuf<16> = FixedBuf.fromRandom(16),
   concatIv: boolean = true,
 ): WebBuf {
-  const encrypted = aescbc_encrypt(data, key.buf, iv.buf);
+  const encrypted = aescbc_encrypt(plaintext, aesKey.buf, iv.buf);
   if (concatIv) {
     return WebBuf.concat([iv.buf, WebBuf.fromUint8Array(encrypted)]);
   }
