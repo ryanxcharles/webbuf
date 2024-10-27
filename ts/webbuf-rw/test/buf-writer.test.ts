ffa66db602c6cae934bd5e8a3ef8ab71cf4c1015
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-27 07:11:34 -0500

    more buf writer tests

--- ts/webbuf-rw/test/buf-writer.test.ts
+++ ts/webbuf-rw/test/buf-writer.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, it, test, expect } from "vitest";
 import { BufWriter } from "../src/buf-writer.js";
-import { U8, U16BE, U32BE, U64BE } from "@webbuf/numbers";
+import { U8, U16BE, U32BE, U64BE, U128BE, U256BE } from "@webbuf/numbers";
 
 describe("BufWriter", () => {
   it("should exist", () => {
@@ -40,12 +40,32 @@ describe("BufWriter", () => {
     });
   });
 
-  describe("writeUint64BEBn", () => {
+  describe("writeUint64BE", () => {
     it("should write an unsigned 64-bit integer in big-endian format", () => {
       const u64: U64BE = U64BE.fromBn(1234567890123456789n);
       bufferWriter.writeU64BE(u64);
       const result = bufferWriter.toBuf();
       expect(result.readBigInt64BE(0)).toEqual(u64.bn);
+    });
+  });
+
+  describe("writeUint128BE", () => {
+    it("should write an unsigned 128-bit integer in big-endian format", () => {
+      const u128: U128BE =
+        U128BE.fromBn(0x0123456789ABCDEF0123456789ABCDEFn);
+      bufferWriter.writeU128BE(u128);
+      const result = bufferWriter.toBuf();
+      expect(result.readBigUint128BE(0)).toEqual(u128.bn);
+    });
+  });
+
+  describe("writeUint256BE", () => {
+    it("should write an unsigned 256-bit integer in big-endian format", () => {
+      const u256: U256BE =
+        U256BE.fromBn(0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEFn);
+      bufferWriter.writeU256BE(u256);
+      const result = bufferWriter.toBuf();
+      expect(result.readBigUint256BE(0)).toEqual(u256.bn);
     });
   });
 });
