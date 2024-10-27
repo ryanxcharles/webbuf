8cb1787a88bade70947b2cb69dd828fba1428713
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-27 15:11:24 -0500

    sanity test

--- ts/webbuf-aescbc-blake3/test/index.test.ts
+++ ts/webbuf-aescbc-blake3/test/index.test.ts
@@ -1,20 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { WebBuf } from "@webbuf/webbuf";
+import{ acb3Encrypt, acb3Decrypt } from "../src/index.js";
 
 describe("Index", () => {
-  it("should encode and decode base64", () => {
-    const myStr = "Hello, World!";
-    const buf = WebBuf.from(myStr);
-    const base64 = buf.toString("base64");
-    const decoded = WebBuf.from(base64, "base64");
-    expect(decoded.toString()).toBe(myStr);
-  });
-
-  it("should encode and decode hex", () => {
-    const myStr = "Hello, World!";
-    const buf = WebBuf.from(myStr);
-    const hex = buf.toString("hex");
-    const decoded = WebBuf.from(hex, "hex");
-    expect(decoded.toString()).toBe(myStr);
+  it("should exist", () => {
+    expect(acb3Encrypt).toBeDefined();
+    expect(acb3Decrypt).toBeDefined();
   });
 });
