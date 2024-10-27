a2fd02ace101596841ae04b7a8dd25a9a0149e25
Ryan X. Charles <ryan@ryanxcharles.com>
2024-10-27 04:55:18 -0500

    add two

--- ts/webbuf-numbers/test/index.test.ts
+++ ts/webbuf-numbers/test/index.test.ts
@@ -2,8 +2,13 @@ import { describe, test, it, expect } from "vitest";
 import { U8, U16, U32, U64, U128, U256 } from "../src/index.js";
 
 describe("U8", () => {
-  it("should create a new U8 instance", () => {
+  it("should create a new U8 instance from number", () => {
     const u8 = U8.fromN(0);
     expect(u8.n).toBe(0);
+  });
+
+  it("should add two U8 instances", () => {
+    const u8 = U8.fromBn(10n).add(U8.fromBn(20n));
+    expect(u8.bn).toBe(30n);
   });
 });
